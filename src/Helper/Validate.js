70f5fbd96663a61fa13d62380dd8450d2cf3b36d
d3f4u1t3d <pravin09032002@gmail.com>
2022-11-15 11:56:38 +0530

    Added DOB validation to the code

--- src/Helper/Validate.js
+++ src/Helper/Validate.js
@@ -14,3 +14,69 @@ export const validatePassword = (password) => {
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
   );
 };
+
+export const validateDOB = (dateSelected, monthSelected, yearSelected) => {
+  let date = dateSelected.toString();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let month = (months.indexOf(monthSelected) + 1).toString();
+
+  let year = yearSelected.toString();
+
+  const dateCheck = /^(0?[1-9]|[12][0-9]|3[01])$/;
+  const monthCheck = /^([1-9]|1[0-2])$/;
+  const yearCheck = /^\d{4}$/;
+
+  if (
+    (month.match(monthCheck) &&
+      date.match(dateCheck) &&
+      year.match(yearCheck)) ||
+    console.log(date, month, year)
+  ) {
+    const ListofDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (month == 1 || month > 2) {
+      if (date > ListofDays[month - 1]) {
+        alert("Invalid date format!");
+        return false;
+      }
+    }
+
+    if (month == 2) {
+      let leapYear = false;
+      if ((!(year % 4) && year % 100) || !(year % 400)) {
+        leapYear = true;
+      }
+
+      if (leapYear == false && date >= 29) {
+        alert("Invalid date format!");
+        return false;
+      }
+
+      if (leapYear == true && date > 29) {
+        alert("Invalid date format!");
+        return false;
+      }
+    }
+    var flag = 1;
+  } else {
+    return false;
+  }
+  if (flag == 1) {
+    return true;
+  }
+};
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,65 +1,157 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './Register.css'
-import InputBox from '../../Components/InputBox/InputBox'
-import Button from '../../Components/Button/Button'
-import DropDown from '../../Components/DropDown/DropDown'
-import {date} from './../../Assets/MockData/mockdata'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
+import InputBox from "../../Components/InputBox/InputBox";
+import Button from "../../Components/Button/Button";
+import DropDown from "../../Components/DropDown/DropDown";
+import { date } from "./../../Assets/MockData/mockdata";
+import {
+  validateEmail,
+  validatePassword,
+  validateDOB,
+} from "../../Helper/Validate";
+import axios from "axios";
 
 function Register() {
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+  const [dateSelected, dateSetSelected] = useState("");
+  const [monthSelected, monthSetSelected] = useState("");
+  const [yearSelected, yearSetSelected] = useState("");
 
-  const [dateSelected,dateSetSelected] = useState("");
-  const [monthSelected,monthSetSelected] = useState("");
-  const [yearSelected,yearSetSelected] = useState("");
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
+  const valdation = (value) => {
+    if (validateEmail(value.email)) {
+      // console.log("email");
+      if (validatePassword(value.password)) {
+        console.log("password");
+        if (validateDOB(dateSelected, monthSelected, yearSelected)) {
+          // console.log(dateSelected, monthSelected, yearSelected);
+          console.log("date");
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (!valdation(formData)) {
+      alert("Wrong Data");
+      return;
+    }
+  };
 
   return (
     <>
-        <div className='RegisterPage'>
-        <div className='container1'>
-      <div className="wrapper1">
-        <div className="textcontainer1">
-        <h1 className='h11'>Create an account</h1>
-        </div>
-        <form className="form1" >
+      <div className="RegisterPage">
+        <div className="container1">
+          <div className="wrapper1">
+            <div className="textcontainer1">
+              <h1 className="h11">Create an account</h1>
+            </div>
+            <form className="form1">
+              <InputBox
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                type="text"
+                htmlFor="Email"
+                Content="EMAIL"
+                className="SpaceRequired"
+              />
 
-          <InputBox type="text" htmlFor="Email" Content="EMAIL" className="SpaceRequired" />
+              <InputBox
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                type="text"
+                htmlFor="Username"
+                Content="USERNAME"
+                className="SpaceRequired"
+              />
 
-          <InputBox type="text" htmlFor="Username" Content="USERNAME" className="SpaceRequired" />
-          
-          <InputBox type="password" htmlFor="Password" Content="PASSWORD" className="SpaceRequired" />
-          <label className='label1'>DATE OF BIRTH
-          </label>
-          
-          <div className='DOBContainer'>
-          <DropDown selected={dateSelected} setSelected={dateSetSelected} option={date.day} defaut="Date"/>
-          <DropDown selected={monthSelected} setSelected={monthSetSelected} option={date.month} defaut="Month" />
-          <DropDown selected={yearSelected} setSelected={yearSetSelected} option={date.year} defaut="Year"/>
-          </div>
+              <InputBox
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                type="password"
+                htmlFor="Password"
+                Content="PASSWORD"
+                className="SpaceRequired"
+              />
+              <label className="label1">DATE OF BIRTH</label>
 
-          {/* <InputBox type="checkbox" htmlFor="emailConfirmation" Content="(Optional) It’s okay to send me emails with Discord updates, tips and special offers. You can opt out at any time." className=".checkbox1" checked/> */}
-          {/* <div style={{display:"flex",flexDirection:"row",justifyContent:'space-between'}}>
+              <div className="DOBContainer">
+                <DropDown
+                  selected={dateSelected}
+                  setSelected={dateSetSelected}
+                  option={date.day}
+                  defaut="Date"
+                />
+                <DropDown
+                  selected={monthSelected}
+                  setSelected={monthSetSelected}
+                  option={date.month}
+                  defaut="Month"
+                />
+                <DropDown
+                  selected={yearSelected}
+                  setSelected={yearSetSelected}
+                  option={date.year}
+                  defaut="Year"
+                />
+              </div>
+
+              {/* <InputBox type="checkbox" htmlFor="emailConfirmation" Content="(Optional) It’s okay to send me emails with Discord updates, tips and special offers. You can opt out at any time." className=".checkbox1" checked/> */}
+              {/* <div style={{display:"flex",flexDirection:"row",justifyContent:'space-between'}}>
           <input className='checkbox1' type="checkbox"/>
           <label className='label2'>(Optional) It’s okay to send me emails with Discord updates, tips and special offers. You can opt out at any time.</label>
           </div> */}
 
-<label class="container2">(Optional) It’s okay to send me emails with Discord updates, tips and special offers. You can opt out at any time.
-  <input type="checkbox" />
-  <span class="checkmark2"></span>
-</label>
+              <label class="container2">
+                (Optional) It’s okay to send me emails with Discord updates,
+                tips and special offers. You can opt out at any time.
+                <input type="checkbox" />
+                <span class="checkmark2"></span>
+              </label>
 
-          <Button type = "submit " value="Continue" className="ButtonLogin" option={date}/>
-          <p className='login'><Link to='/login'>Already have an account?</Link></p>
+              <Button
+                onClick={handleForm}
+                type="submit "
+                value="Continue"
+                className="ButtonLogin"
+                option={date}
+              />
+              <p className="login">
+                <Link to="/login">Already have an account?</Link>
+              </p>
 
-          <p className='disclaimer'>By registering you agree to Discord's<a href='#'>Terms of Service</a> and <a href='#'>Privacy Policy</a> </p>
-        </form>
+              <p className="disclaimer">
+                By registering you agree to Discord's
+                <a href="#">Terms of Service</a>
+                <a href="#">Privacy Policy</a>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
- 
     </>
-  )
+  );
 }
 
-export default Register
+export default Register;
